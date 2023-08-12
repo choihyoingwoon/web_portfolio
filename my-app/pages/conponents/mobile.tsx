f6ab6fa9adb7267c5ba5ec26e51607cd4c4f57da
choihyoingwoon <cpobo@hanmail.net>
2023-08-12 18:06:17 +0900

    모바일 페이지 만듬

---
+++ my-app/pages/conponents/mobile.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Mobile: React.FC = () => {
+  return (
+    <div className="w-full h-screen bg-white">
+      <p>만드는중</p>
+    </div>
+  );
+};
+
+export default Mobile;
