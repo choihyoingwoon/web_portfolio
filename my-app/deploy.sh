9e1c884f900f16e53bec58ea7971012b9214a1ac
choihyoingwoon <cpobo@hanmail.net>
2023-08-16 00:48:47 +0900

    Deploy to GitHub Pages

--- my-app/deploy.sh
+++ my-app/deploy.sh
@@ -1,8 +1,13 @@
-npm run build
+# 빌드
+npm run export
 
-# 배포 스크립트 실행
-git checkout -b gh-pages
-git add out
+# 빌드된 정적 파일 이동
+mv out web_portfolio
+
+# 커밋 및 푸시
+git add .
 git commit -m "Deploy to GitHub Pages"
-git subtree push --prefix out origin gh-pages
-git checkout main
+git push origin gh-pages
+
+# 작업 디렉터리로 돌아가기
+cd ..
