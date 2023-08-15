e0f58742b91ea88830d020491d37075eedec5380
choihyoingwoon <cpobo@hanmail.net>
2023-08-16 00:30:45 +0900

    deploy to gh-pages

--- my-app/next.config.js
+++ my-app/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  basePath: "/web_portfolio",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
